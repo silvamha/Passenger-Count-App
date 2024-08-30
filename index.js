b4377a48614b2e64611e35f468f5fce61de6b271
Ed <silva.email@yahoo.com>
2024-08-29 21:29:10 -0500

    feat: Add bonus points functionality to index.js

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ incrementBtn.addEventListener("click", function () {
     count += 1;
     countEl.textContent = count;
 });
+
+
+let bonusPoints = 50;
+bonusPoints=bonusPoints+50;
+console.log(bonusPoints);
+
+bonusPoints=bonusPoints-75;
+console.log(bonusPoints);
+
+bonusPoints=bonusPoints+45;
+console.log(bonusPoints)
