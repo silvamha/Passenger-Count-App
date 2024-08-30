1bc34f513786111d7f036207261595f475c10504
Ed <silva.email@yahoo.com>
2024-08-30 04:59:26 -0500

    Completed functions exercises

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ incrementBtn.addEventListener("click", function () {
   console.log(count);
 });
 
+function showNumber() {
+  let num = 42;
+  console.log(num);
+}
+
+showNumber();
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+function addLaps() {
+
+  let totalLaps = lap1 + lap2 + lap3;
+  console.log(totalLaps);
+}
+
+addLaps()
+
+let lapsCompleted = 0;
+
+function incrementLap() {
+  lapsCompleted += 1;
+  console.log(lapsCompleted);
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
