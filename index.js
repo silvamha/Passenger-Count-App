d3458f6543b7745a976764481045f4d437b6434a
Ed <silva.email@yahoo.com>
2024-08-29 21:37:30 -0500

    Refactor bonus points functionality in index.js

--- index.js
+++ index.js
@@ -11,12 +11,3 @@ incrementBtn.addEventListener("click", function () {
 });
 
 
-let bonusPoints = 50;
-bonusPoints=bonusPoints+50;
-console.log(bonusPoints);
-
-bonusPoints=bonusPoints-75;
-console.log(bonusPoints);
-
-bonusPoints=bonusPoints+45;
-console.log(bonusPoints)
