291777342da6438baa6201547e1b6fb42ef5e29b
Ed <silva.email@yahoo.com>
2024-08-29 21:15:45 -0500

    Refactor JavaScript code to dynamically update count element

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // document.getElementById("count-el").innerText = 5;
+const countEl = document.getElementById("count-el");
+const incrementBtn = document.getElementById("increment-btn");
+const saveBtn = document.getElementById("save-btn");
+let count = 0;
+console.log(count);
 
-let count= 0
-console.log(count)
-
+incrementBtn.addEventListener("click", function () {
+    count += 1;
+    countEl.textContent = count;
+});
